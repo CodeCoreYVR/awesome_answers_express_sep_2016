5067f3d19b4d16006b5594b24e8c903c6b7131db
Steve Godin <steve.god@gmail.com>
2016-11-17 10:20:13 -0800

    Persist Questions with Mongo

---
+++ models/question.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+  title: {type: String, trim: true, require: true},
+  description: {type: String, trim: true, require: true}
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
--- routes/questions.js
+++ routes/questions.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router  = express.Router();
+var Question = require('../models/question');
 
 router.get('/new', function(req, res, next){
   res.render('questions/new');
 });
 
 router.post('/', function(req, res, next){
-  res.end('creating question action');
+  var question = new Question({
+    title: req.body.title,
+    description: req.body.description
+  })
+
+  question.save(function (err, question) {
+    if (err) {
+      res.render('questions/new', {errors: err});
+    } else {
+      res.end(`Created Question with id: ${question.id}`)
+    }
+  })
 });
 
 module.exports = router;
