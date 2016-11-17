a9a16b40a73b170800407726e6cb356c80c7059b
Steve Godin <steve.god@gmail.com>
2016-11-17 10:55:05 -0800

    Add index route for Questions

--- models/question.js
+++ models/question.js
@@ -4,6 +4,8 @@ var Schema = mongoose.Schema;
 var QuestionSchema = new Schema({
   title: {type: String, trim: true, require: true},
   description: {type: String, trim: true, require: true}
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
--- routes/questions.js
+++ routes/questions.js
@@ -6,6 +6,19 @@ router.get('/new', function(req, res, next){
   res.render('questions/new');
 });
 
+router.get('/', function(req, res, next) {
+  // We can query Mongo with Mongoose by using the
+  // .find method (there are many others). If we give find's
+  // first argument and empty object, it will get all the questions
+  Question.find({}).sort({createdAt: -1}).exec(function(err, questions) {
+    if (err) {
+      next();
+    } else {
+      res.render('questions/', {questions: questions})
+    }
+  })
+})
+
 router.post('/', function(req, res, next){
   var question = new Question({
     title: req.body.title,
@@ -16,7 +29,9 @@ router.post('/', function(req, res, next){
     if (err) {
       res.render('questions/new', {errors: err});
     } else {
-      res.end(`Created Question with id: ${question.id}`)
+      // Send the user to the question index if save is
+      // successful
+      res.redirect('/questions');
     }
   })
 });
